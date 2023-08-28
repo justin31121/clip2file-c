2796b0b4280b9ef6e64b481561984543ddb550c9
Justin Schartner <justin.schartner@tu-dortmund.de>
2023-08-28 20:13:17 +0200

    msvc build

--- build.bat
+++ build.bat
@@ -1,1 +1,21 @@
-gcc -Wall -Wextra -pedantic -ggdb -o bin\clip2file src\main.c 
+@echo off
+
+mkdir bin 2> NUL > NUL
+
+where /q cl
+IF errorlevel 1 (
+    where /q gcc
+    IF errorlevel 1 (
+        echo "ERROR: Make sure to have msvc or mingw installed!"
+        @echo on
+        exit /b
+    ) ELSE (
+        gcc -Wall -Wextra -pedantic -ggdb -o bin\clip2file src\main.c 
+    )
+) ELSE (    
+    cl /W4 /Fe:bin\clip2file src\main.c user32.lib
+)
+
+@echo on
+
+
